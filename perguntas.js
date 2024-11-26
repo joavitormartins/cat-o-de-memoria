bcd2a5ccd02eedb2f9f05fd041611ad8d72f88cf
JoãoVitor <99548321+joavitormartins@users.noreply.github.com>
2024-11-26 09:53:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Corinthians',
+    'Quantas vitorias seguidas ele tem no brasileirão em 2024?',
+    '6 vitorias seguidas'
+)
+
+criaCartao(
+    'Estadio do Corinthians',
+    'Onde fica localizado o estadio do Corinthians?',
+    'Itaquera-sp'
+)
+
+criaCartao(
+    'Estadio',
+    'Qual o nome do estadio do Corinthians? ',
+    'neo química arena'
+)
+
+criaCartao(
+    'Estadio',
+    'Qual a capacidade do estadio do Corinthians',
+    '49.205'
+)
